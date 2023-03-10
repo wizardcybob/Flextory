59c11b49dcb225f1ecaf9035ea3fbdcfbca0894c
Romain Bauer <romain.bauer@t-med.fr>
2023-03-10 17:21:21 +0100

    fe - added input component

--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,3 +1,23 @@
-@props(['disabled' => false])
+@props(['placeholder', 'label', 'icon' => null])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-0 text-secondary-dark border-t-[0px] border-x-[0px] border-b-[4px] border-b-primary focus:border-b-primary-dark rounded-t-md overflow-hidden bg-secondary-light']) !!}>
+<div class="flex items-center mt-0 text-secondary-dark border-t-[0px] border-x-[0px] border-b-[4px] border-b-primary focus:border-b-primary-dark rounded-t-md bg-secondary-light relative">
+    @if ($icon)
+    <p class="pl-3">{{$icon}}</p>
+    @endif
+    <input {!! $attributes->merge(['class' => 'px-3 group md:text-xl w-full bg-inherit border-none focus:ring-0', 'aria-labelledby' => $placeholder, 'placeholder' => $placeholder]) !!}>
+    @if ($icon)
+    <label class="absolute -top-3 left-10 text-primary font-semibold">
+        {{ $label }}
+        @if ($attributes->get('required'))
+        <span>*</span>
+        @endif
+    </label>
+    @else
+    <label class="absolute -top-3 left-6 text-primary font-semibold">
+        {{ $label }}
+        @if ($attributes->get('required'))
+        <span>*</span>
+        @endif
+    </label>
+    @endif
+</div>
