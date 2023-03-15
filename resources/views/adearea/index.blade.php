0d6554168f59d5a8fae504b2041703255f52ea85
Chloé JACOB <chloe.jacob4@etu.unistra.fr>
2023-03-15 09:55:28 +0100

    FE - quelques modifications

--- resources/views/adearea/index.blade.php
+++ resources/views/adearea/index.blade.php
@@ -7,12 +7,15 @@
 
     {{-- VIEW --}}
     <div class="w-fit mx-auto flex flex-col gap-8">
+        <div class="flex justify-center">
+            <a href="{{ route('sheet.create') }}" class="btn_primary w-fit" title="Ajouter une fiche d'amélioration"><i class="fa-solid fa-plus" aria-hidden="true"></i>Ajouter une fiche d'amélioration</a>
+        </div>
         @if ($adeareas->isNotEmpty())
         <ul class="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-3 md:gap-4">
             @foreach ($adeareas as $adearea)
             <li>
-                <div class="bg-secondary-dark h-full p-5 sm:p-3 md:p-5" style="background-image: url({{ asset('storage/images/flextory_login.jpg') }}); background-size:cover;"> <!--350px-->
-                    <div class="p-5 sm:p-3 md:p-5 backdrop-blur-md bg-white/30 flex flex-col items-center justify-center gap-5 h-full">
+                <div class="bg-secondary-dark h-full p-4 sm:p-3 md:p-5" style="background-image: url({{ asset('storage/images/flextory_login.jpg') }}); background-size:cover;"> <!--350px-->
+                    <div class="p-4 sm:p-3 md:p-5 backdrop-blur-md bg-white/40 flex flex-col items-center justify-center gap-5 h-full">
                         <p class="sous-titre_page uppercase">{{ $adearea->name }}</p>
                         <ul class="grid grid-cols-1 sm:grid-cols-2 gap-2 w-full">
                             @foreach ($adearea->areas as $area)
