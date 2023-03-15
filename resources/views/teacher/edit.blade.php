1ca63a711de78fb8d2bccfc286b4db47b1230089
Romain Bauer <romain.bauer@t-med.fr>
2023-03-15 18:04:10 +0100

    fe - created all teachers pages

--- resources/views/teacher/edit.blade.php
+++ resources/views/teacher/edit.blade.php
@@ -1,39 +1,72 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Modifier l\'enseignant') }}
+            {{ __('Créer un enseignant') }}
         </h2>
     </x-slot>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <form action="{{ route('teacher.update', ['teacher' => $teacher]) }}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <p><input aria-labelledby="Nom" type="text" placeholder="Nom" name="name" value="{{ $teacher->name }}"></p>
-                    <p><input aria-labelledby="Permanent" type="text" placeholder="Permanent" name="permanent" value="{{ $teacher->permanent }}"></p>
-                    <p>
-                        <select name="department_id">
-                            <option value=""></option>
-                            @foreach ($departments as $department)
-                                <option value="{{ $department->id }}" @if ($teacher->department_id == $department->id) selected @endif>
-                                    {{ $department->name }}</option>
-                            @endforeach
-                        </select>
-                    </p>
-                    <p>
-                        <select name="status_id">
-                            <option value=""></option>
-                            @foreach ($statuses as $status)
-                                <option value="{{ $status->id }}" @if ($teacher->status_id == $status->id) selected @endif>
-                                    {{ $status->name }}</option>
-                            @endforeach
-                        </select>
-                    </p>
 
-                    <p><button aria-labelledby="Valider" type="submit">Valider</button></p>
-                </form>
+    {{-- VIEW --}}
+    <div class="w-full mx-auto flex flex-col gap-8">
+        <h1 class="titre_page">Création d'un enseignant</h1>
+        <button class="btn_primary w-fit" onclick="window.history.back()" role="button" aria-label="Retour à la page précédente" title="Retour à la page précédente"><i class="fa-solid fa-chevron-left" aria-hidden="true"></i>
+            Retour</button>
+
+        {{-- FORMULAIRE --}}
+        <form action="{{ route('teacher.update', ['teacher' => $teacher]) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            {{-- Nom --}}
+            <div class="relative">
+                <label for="name" id="name-label" class="absolute label_form">Nom<span class="text-tertiary">*</span> :</label>
+                <input type="text" id="name" name="name" class="input_textarea_form" placeholder="Nom" value="{{ $teacher->name }}" aria-labelledby="name-label" required aria-required="true">
             </div>
-        </div>
+
+            {{-- Permanent --}}
+            <div class="relative text-primary-darker">
+                <label for="permanent" class="font-semibold mb-2" id="permanent-label">Enseignant :</label>
+                <div class="flex items-center gap-8">
+                    <div>
+                        <label for="permanent-true" id="permanent-true-label">Permanent</label>
+                        <input value="1" type="radio" id="permanent-true" name="permanent" class="checkbox_form ml-2" aria-labelledby="permanent-true-label" @if ($teacher->permanent === 1) checked @endif>
+                    </div>
+                    <div>
+                        <label for="permanent-false" id="permanent-false-label">Non permanent</label>
+                        <input value="2" type="radio" id="permanent-false" name="permanent" class="checkbox_form ml-2" aria-labelledby="permanent-false-label" @if ($teacher->permanent === 2) checked @endif>
+                    </div>
+                </div>
+            </div>
+
+            {{-- Departements --}}
+            <div class="relative w-full mt-4">
+                <label for="department" id="department-label" class="absolute label_form">Département<span class="text-tertiary">*</span> :</label>
+                <select class="select_form" id="department" name="department_id" aria-labelledby="department-label" required aria-required="true">
+                    <option value="" selected disabled hidden>Sélectionner un département</option>
+                    @foreach ($departments as $department)
+                        <option value="{{ $department->id }}" @if ($teacher->department_id === $department->id) selected @endif>
+                            {{ $department->name }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+
+            {{-- Statuts --}}
+            <div class="relative w-full mt-4">
+                <label for="status" id="status-label" class="absolute label_form">Statuts<span class="text-tertiary">*</span> :</label>
+                <select class="select_form" id="status" name="status_id" aria-labelledby="status-label" required aria-required="true">
+                    <option value="" selected disabled hidden>Sélectionner un statut</option>
+                    @foreach ($statuses as $status)
+                        <option value="{{ $status->id }}" @if ($teacher->status_id === $status->id) selected @endif>
+                            {{ $status->name }}
+                        </option>
+                    @endforeach
+                </select>
+
+            <p class="text-tertiary text-xs mt-4">*Champs obligatoires.</p>
+
+            <div class="flex justify-end w-full">
+                <button class="btn_primary flex justify-center" type="submit" aria-label="Enregistrer les modifications">Enregistrer</button>
+            </div>
+        </form>
     </div>
 </x-app-layout>
--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -5,26 +5,56 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <p><a aria-labelledby="Créer un nouvel enseignant" href="{{ route('teacher.create') }}">Créer un nouvel enseignant</a></p>
+    <div class="w-full mx-auto flex flex-col gap-8">
+        <h1 class="titre_page">Enseignants</h1>
+        <a href="javascript:history.go(-1)" class="btn_primary w-fit" title="Retour à la page précédente"><i class="fa-solid fa-chevron-left" aria-hidden="true"></i>Retour</a>
 
-                @if ($teachers->isNotEmpty())
-                    <ul>
-                        @foreach ($teachers as $teacher)
-                                <li><a href="{{ route('teacher.show', $teacher) }}">{{ $teacher->name }}</a></li>
-                                <p><a aria-labelledby="Modifier" href="{{ route('teacher.edit', ['teacher' => $teacher->id])}}">Modifier</a></p>
+        <div class="flex flex-col gap-4">
+            @if ($teachers->isNotEmpty())
+            <ul class="border-2 border-primary rounded overflow-hidden max-h-[500px] overflow-y-scroll">
+                @php
+                    $bgClass = 'bg-secondary';
+                @endphp
 
-                                <form action="{{ route('teacher.destroy', ['teacher' => $teacher->id]) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button aria-labelledby="Supprimer" type="submit">Supprimer</button>
-                                </form>
-                        @endforeach
-                    </ul>
-                @endif
-            </div>
+                @foreach ($teachers as $index => $teacher)
+                    <li class="{{ $bgClass }} flex flex-col gap-2 md:gap-3 items-center justify-center md:flex-row md:justify-between md:items-center min-h-12 px-3 py-[10px]">
+                        {{-- textes --}}
+                        <div class="flex flex-col md:flex-row items-center flex-wrap gap-1 md:gap-2">
+                            <p class="font-semibold text-center md:text-left">{{ $teacher->name }}</p>
+                        </div>
+                        {{-- all btns --}}
+                        <div class="flex flex-row items-center gap-2">
+                            <div class="flex gap-2">
+                                {{-- btn view --}}
+                                <a class="bg-view hover:bg-view-dark text-white py-1 px-4 rounded w-fit h-fit" href="{{ route('teacher.show', $teacher) }}" aria-label="Voir les informations de {{ $teacher->name }}"><i class="fa-solid fa-eye" aria-hidden="true"></i></a>
+                                {{-- btn edit --}}
+                                <a class="bg-edit hover:bg-edit-dark text-white py-1 px-2 rounded w-fit h-fit" href="{{ route('teacher.edit', ['teacher' => $teacher->id])}}" aria-label="Modifier {{ $teacher->name }}"><i class="fa-solid fa-pen-to-square" aria-hidden="true"></i></a>
+                                {{-- btn delete --}}
+                                @if (Auth::user()->role === 1 || Auth::user()->role === 2)
+                                    <form action="{{ route('teacher.destroy', ['teacher' => $teacher->id]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="bg-delete hover:bg-delete-dark text-white py-1 px-2 rounded w-fit h-fit" aria-label="Supprimer {{ $teacher->name }}" type="submit"><i class="fa-solid fa-trash"></i></button>
+                                    </form>
+                                @endif
+
+                            </div>
+                        </div>
+                    </li>
+                    @php
+                        $bgClass = ($bgClass == 'bg-secondary') ? 'bg-secondary-light' : 'bg-secondary';
+                    @endphp
+                @endforeach
+            </ul>
+            @else
+                <div class="border-2 border-primary rounded overflow-hidden min-h-[200px] flex justify-center items-center">
+                    <p class="text-xl font-semibold text-tertiary">Aucun résultat :(</p>
+                </div>
+            @endif
+
+
+
+            <a href="{{ route('teacher.create') }}" class="btn_primary w-full" title="Ajouter un enseignant"><i class="fa-solid fa-plus" aria-hidden="true"></i>Ajouter un enseignant</a>
         </div>
     </div>
 </x-app-layout>
