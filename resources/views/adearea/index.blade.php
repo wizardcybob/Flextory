6b9918f4080287770f06bc686d770af00e8607e8
KRATZ RAPHAEL <raphael.kratz2@etu.unistra.fr>
2023-03-16 11:35:31 +0100

    Ajout bouton "créer sous-zone"

--- resources/views/adearea/index.blade.php
+++ resources/views/adearea/index.blade.php
@@ -9,6 +9,7 @@
     <div class="w-fit mx-auto flex flex-col gap-8">
         <div class="flex justify-center">
             <a href="{{ route('sheet.create') }}" class="btn_primary w-fit" title="Ajouter une fiche d'amélioration"><i class="fa-solid fa-plus" aria-hidden="true"></i>Ajouter une fiche d'amélioration</a>
+            <a href="{{ route('area.create') }}" class="btn_primary w-fit" title="Ajouter une sous-zone"><i class="fa-solid fa-plus" aria-hidden="true"></i>Ajouter une sous-zone</a>
         </div>
         @if ($adeareas->isNotEmpty())
         <ul class="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-3 md:gap-4">
